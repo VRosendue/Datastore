16db448d6fe038a4e2fbe52ea4192bf640019660
VRosendue <110988416+VRosendue@users.noreply.github.com>
2022-08-31 14:21:34 +0200

    Add files via upload

---
+++ DataStoreProject/src/main/java/com/postgres/models/dto/CharactersDTO.java
@@ -0,0 +1,15 @@
+package com.postgres.models.dto;
+
+import java.util.Set;
+
+public class CharactersDTO {
+			
+		private int character_id;
+		private String fullName;
+		private String alias;
+		private String gender;
+		private Set<Integer> movies;
+	}
+
+
+
