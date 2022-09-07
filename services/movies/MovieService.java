971824685870189fd4dec2be891edad2dca5dd09
MGylden <99180585+MGylden@users.noreply.github.com>
2022-09-07 16:27:09 +0200

    Delete services directory

--- services/movies/MovieService.java
+++
@@ -1,9 +0,0 @@
-package com.postgres.services.movies;
-
-import com.postgres.models.Movies;
-import com.postgres.models.dto.MovieDTO;
-import com.postgres.services.CrudService;
-
-public interface MovieService extends CrudService<MovieDTO, Integer> {
-
-}
