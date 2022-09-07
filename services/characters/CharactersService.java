0d3247414e2d22f30fb4e8ac142ddf87525e2b7e
MGylden <99180585+MGylden@users.noreply.github.com>
2022-09-07 16:24:36 +0200

    Delete services directory

--- services/characters/CharactersService.java
+++
@@ -1,9 +0,0 @@
-package com.postgres.services.characters;
-
-import com.postgres.services.CrudService;
-import com.postgres.models.*;
-
-public interface CharactersService extends CrudService<Characters, Integer> {
-	
-
-}
--- services/franchise/FranchiseService.java
+++
@@ -1,9 +0,0 @@
-package com.postgres.services.franchise;
-
-import com.postgres.services.CrudService;
-import com.postgres.models.*;
-
-public interface FranchiseService extends CrudService<Franchise, Integer> {
-	
-
-}
